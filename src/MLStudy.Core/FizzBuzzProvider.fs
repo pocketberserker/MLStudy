9a0a2795d5f8915cab89a5badf65d3abe17b1cf1
pocketberserker <pocketberserker@gmail.com>
2016-07-09 09:41:23 +0900

    とりあえず生成まで

---
+++ src/MLStudy.Core/FizzBuzzProvider.fs
@@ -0,0 +1,55 @@
+﻿namespace MLStudy.TypeProviders
+
+open System.IO
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+open FSharp.Quotations
+
+[<TypeProvider>]
+type FizzBuzzProvider(cfg: TypeProviderConfig) as this =
+  inherit TypeProviderForNamespaces()
+
+  let asm = Assembly.GetExecutingAssembly()
+  let ns = "MLStudy.TypeProviders"
+
+  let typ = ProvidedTypeDefinition(asm, ns, "FizzBuzzProvider", Some typeof<obj>)
+  do
+    let parameters = [
+      ProvidedStaticParameter("first", typeof<int>)
+      ProvidedStaticParameter("last", typeof<int>)
+    ]
+    typ.DefineStaticParameters(parameters, fun typeName (args: obj[]) ->
+      let first = unbox<int> args.[0]
+      let last = unbox<int> args.[1]
+      let typ = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>, HideObjectMethods = true)
+      for n in [first .. last] do
+        let fizzbuzz =
+          match (n % 3, n % 5) with
+          | (0, 0) -> "FizzBuzz"
+          | (0, _) -> "Fizz"
+          | (_, 0) -> "Buzz"
+          | _ -> string n
+        typ.AddMembersDelayed(fun () ->
+          let numType = ProvidedTypeDefinition(sprintf "_%d" n, Some typeof<obj>)
+          let ctor = ProvidedConstructor(parameters = [], InvokeCode = fun _ -> <@@ () @@>)
+          numType.AddMember(ctor)
+          numType.AddMemberDelayed(fun () -> ProvidedProperty("FizzBuzz", typeof<string>, GetterCode = fun _ -> <@@ fizzbuzz @@>))
+          let prop =
+            ProvidedProperty(
+              propertyName = string n,
+              IsStatic = true,
+              propertyType = numType,
+              GetterCode = (fun _ -> Expr.NewObject(ctor, []))
+            )
+          [
+            numType :> MemberInfo
+            prop :> MemberInfo
+          ]
+        )
+      typ
+    )
+    this.AddNamespace(ns, [typ])
+
+[<assembly:TypeProviderAssembly>]
+do ()
--- src/MLStudy/Program.fs
+++ src/MLStudy/Program.fs
@@ -1,5 +1,16 @@
 ﻿module MLStudy.Program
 
+open MLStudy.TypeProviders
+
+let inline dump (value: ^n) = (^n: (member FizzBuzz: string) value) |> printfn "%s"
+
+type FizzBuzz = FizzBuzzProvider<1, 100>
+
 [<EntryPoint>]
 let main _ = 
+  dump FizzBuzz.``1``
+  dump FizzBuzz.``2``
+  dump FizzBuzz.``3``
+  dump FizzBuzz.``4``
+  dump FizzBuzz.``5``
   0
